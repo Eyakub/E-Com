f1feef2884ea01d8d9d969fe23a15431dee09dc2
Eyakub Sorkar <eyakub.tryonyx@gmail.com>
2019-03-16 18:39:20 +0600

    #placeholder in image fadein

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -62,7 +62,12 @@ class ProductCard extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          Image.network(product.image),
+          FadeInImage(
+            image: NetworkImage(product.image),
+            height: 300.0,
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/background.jgp'),
+          ),
           _buildTitlePriceRow(),
           AddressTag('UniSon Square, San Francisco'),
           new Text(product.userEmail ?? ''),
